6fe4ddfe41499525040f528a1ef0e3106cca48d3
zekguevarra <zekguevarra1053@gmail.com>
2022-09-27 12:32:25 +0800

    preloader included

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+//slidecontainer
+
+
+//preloader
+var loader;
+function loadNow(opacity) {
+  if (opacity <= 0) {
+    displayContent();
+  } else {
+    loader.style.opacity = opacity;
+    window.setTimeout(function () {
+      loadNow(opacity -1);
+    }, 2000);
+  }
+}
+function displayContent() {
+  loader.style.display = 'none';
+  document.getElementById('content').style.display = 'block';
+}
+document.addEventListener('DOMContentLoaded', function () {
+  loader = document.getElementById('loader');
+  loadNow(1);
+});
+//hero animation
 (function() {
 
     var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
